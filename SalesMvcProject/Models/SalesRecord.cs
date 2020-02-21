40b340af71a8720894ba202da558de430c70d453
Marcelo Alves <alves-marcelo97@hotmail.com>
2020-02-21 11:59:25 -0300

    adding seeding service

--- SalesMvcProject/Models/SalesRecord.cs
+++ SalesMvcProject/Models/SalesRecord.cs
@@ -9,16 +9,18 @@ namespace SalesMvcProject.Models
         public DateTime Date { get; set; }
         public double Amount { get; set; }
         public eSalesStatus Status { get; set; }
+        public Seller Seller { get; set; }
 
         public SalesRecord()
         {
         }
-        public SalesRecord(int id, DateTime date, double amount, eSalesStatus status)
+        public SalesRecord(int id, DateTime date, double amount, eSalesStatus status, Seller seller)
         {
             Id = id;
             Date = date;
             Amount = amount;
             Status = status;
+            Seller = seller;
         }
     }
 }
