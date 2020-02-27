c1a6e7f90de70a8ca66e98c578601f36d8d55652
Marcelo Alves <alves-marcelo97@hotmail.com>
2020-02-27 09:39:59 -0300

    foreign key not null

--- SalesMvcProject/Migrations/20200227122757_DepForeignKey.Designer.cs
+++ SalesMvcProject/Migrations/20200227122757_DepForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesMvcProject.Models;
 
 namespace SalesMvcProject.Migrations
 {
     [DbContext(typeof(SalesMvcProjectContext))]
-    partial class SalesMvcProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20200227122757_DepForeignKey")]
+    partial class DepForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SalesMvcProject/Migrations/20200227122757_DepForeignKey.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesMvcProject.Migrations
+{
+    public partial class DepForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
